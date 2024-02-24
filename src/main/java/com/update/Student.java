094697d8846b893a4f9f65a3c17b380d2c49e219
Nikita Shriwas <nikitashriwas98@gmail.com>
2024-02-24 21:39:21 +0530

    added hibernate update file

---
+++ src/main/java/com/update/Student.java
@@ -0,0 +1,46 @@
+package com.update;
+
+import javax.persistence.*;;
+
+@Entity
+@Table(name = "stud")
+public class Student {
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "city")
+	private String city;
+	@Column(name = "mobile")
+	private String mobile;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getMobile() {
+		return mobile;
+	}
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/update/Test.java
@@ -0,0 +1,27 @@
+package com.update;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory sessionFactory = cfg.buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		Transaction t = session.beginTransaction();
+		
+		//update query for mavan hibernate
+		Student student = (Student) session.get(Student.class, 1);
+		student.setName("jyoti");
+		session.update(student);
+		session.getTransaction().commit();
+		session.close();
+		sessionFactory.close();
+		System.out.println("Record update successfully");
+	}
+
+}
